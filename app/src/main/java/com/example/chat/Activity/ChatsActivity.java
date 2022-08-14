999816c4c313f8e0413802990127de7ab83c0a64
Elking <ga71387@gmail.com>
2022-08-14 10:50:24 -0400

    Updating

--- app/src/main/java/com/example/chat/Activity/ChatsActivity.java
+++ app/src/main/java/com/example/chat/Activity/ChatsActivity.java
@@ -2,17 +2,38 @@ package com.example.chat.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.PerformanceHintManager;
+import android.preference.PreferenceManager;
+import android.util.Base64;
 
 import com.example.chat.R;
+import com.example.chat.adapter.ChatAdapter;
 import com.example.chat.databinding.ActivityChatsBinding;
+import com.example.chat.models.ChatMessage;
 import com.example.chat.models.User;
 import com.example.chat.utilities.Constants;
+import com.example.chat.utilities.PreferenceManger;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.time.DateTimeException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 public class ChatsActivity extends AppCompatActivity {
 
     private ActivityChatsBinding binding;
     private User receiverUser;
+    private List<ChatMessage> chatMessage;
+    private ChatAdapter chatAdapter;
+    private PreferenceManger preferenceManger;
+    private FirebaseFirestore database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +42,34 @@ public class ChatsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setListeners();
         loadReceiverDetails();
+        init();
+    }
+
+    private void init(){
+        preferenceManger = new PreferenceManger(getApplicationContext());
+        chatMessage = new ArrayList<>();
+        chatAdapter = new ChatAdapter(
+                chatMessage,
+                getBitmapFromEncodedString(receiverUser.image),
+                preferenceManger.getString(Constants.KEY_USER_ID)
+        );
+        binding.chatRecyclerview.setAdapter(chatAdapter);
+        database = FirebaseFirestore.getInstance();
+    }
+
+    private void sendMessage(){
+        HashMap<String, Object> message = new HashMap<>();
+        message.put(Constants.KEY_SENDER_ID , preferenceManger.getString(Constants.KEY_USER_ID));
+        message.put(Constants.KEY_RECEIVER_ID , receiverUser.id);
+        message.put(Constants.KEY_MESSAGE , binding.inputMessage.getText().toString());
+        message.put(Constants.KEY_TIMESTAMP , new Date());
+        database.collection(Constants.KEY_COLLECTION_CHAT).add(message);
+        binding.inputMessage.setText(null);
+    }
+
+    private Bitmap getBitmapFromEncodedString(String  encodedImage){
+        byte[] bytes = Base64.decode(encodedImage , Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
     }
 
     private void loadReceiverDetails(){
@@ -30,5 +79,6 @@ public class ChatsActivity extends AppCompatActivity {
 
     private void setListeners(){
         binding.imageBack.setOnClickListener(v -> onBackPressed());
+        binding.layoutSend.setOnClickListener(v -> sendMessage());
     }
 }
--- app/src/main/java/com/example/chat/utilities/Constants.java
+++ app/src/main/java/com/example/chat/utilities/Constants.java
@@ -13,5 +13,9 @@ public class Constants {
     public static final String KEY_FCM_TOKEN = "fcmToken";
     public static final String KEY_USER = "user";
     public static final int VIEW_TYPE_SENT = 1;
-    public static final int VIEW_TYPE_RECEIVED = 2;
+    public static final String KEY_COLLECTION_CHAT = "chat";
+    public static final String KEY_SENDER_ID = "senderId";
+    public static final String KEY_RECEIVER_ID = "receiverId";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_TIMESTAMP = "timesTamp";
 }
